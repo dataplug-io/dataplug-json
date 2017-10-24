54ac82236c38ec88bf07f7eab4e7a0f3e9da975c
Alexey Pelykh <alexey.pelykh@gmail.com>
2017-10-24 07:28:34 +0300

    Add basic test

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,3 +1,24 @@
-const chai = require('chai')
+/* eslint-env node, mocha */
+require('chai')
+  .should()
+const dataplugJson = require('../lib')
 
-chai.should()
+describe('dataplug-json', () => {
+  it('should have "JsonSequentialStreamsReader" class', () => {
+    dataplugJson
+      .should.have.property('JsonSequentialStreamsReader')
+      .that.is.an('function')
+  })
+
+  it('should have "JsonStreamReader" class', () => {
+    dataplugJson
+      .should.have.property('JsonStreamReader')
+      .that.is.an('function')
+  })
+
+  it('should have "JsonStreamWriter" class', () => {
+    dataplugJson
+      .should.have.property('JsonStreamWriter')
+      .that.is.an('function')
+  })
+})
