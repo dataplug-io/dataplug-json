1c788c21b41a1947fecf43f90ca619178ead4512
Alexey Pelykh <alexey.pelykh@gmail.com>
2017-10-26 13:48:25 +0300

    Fix and more tests

--- test/jsonStreamReader.spec.js
+++ test/jsonStreamReader.spec.js
@@ -1,10 +1,34 @@
 /* eslint-env node, mocha */
-require('chai')
+const should = require('chai')
   .should()
+const { PassThrough } = require('stream')
 const { JsonStreamReader } = require('../lib')
 
 describe('JsonStreamReader', () => {
-  it('transforms json to object', () => {
+  it('transforms empty JSON to null', () => {
+    const reader = new JsonStreamReader()
+    reader.end()
+    const data = reader.read()
+    should.not.exist(data)
+  })
+
+  it('transforms empty JSON array to null', () => {
+    const reader = new JsonStreamReader()
+    reader.write('[]')
+    reader.end()
+    const data = reader.read()
+    should.not.exist(data)
+  })
+
+  it('transforms simple JSON to object', () => {
+    const reader = new JsonStreamReader()
+    reader.write('[{"property":"value"}]')
+    reader.end()
+    reader.read()
+      .should.be.deep.equal({property: 'value'})
+  })
+
+  it('transforms complex JSON to object', () => {
     const object = {
       $attr: 'attrValue',
       subObject: {
@@ -16,5 +40,34 @@ describe('JsonStreamReader', () => {
     reader.end()
     reader.read()
       .should.be.deep.equal(object)
+  })
+
+  it('supports chaining without data', () => {
+    const sourceStream = new PassThrough()
+    const reader = new JsonStreamReader()
+    const targetStream = new PassThrough({ objectMode: true })
+
+    sourceStream
+      .pipe(reader)
+      .pipe(targetStream)
+    sourceStream.end()
+
+    const data = targetStream.read()
+    should.not.exist(data)
+  })
+
+  it('supports chaining with data', () => {
+    const sourceStream = new PassThrough()
+    const reader = new JsonStreamReader()
+    const targetStream = new PassThrough({ objectMode: true })
+
+    sourceStream
+      .pipe(reader)
+      .pipe(targetStream)
+    sourceStream.write('[{"property":"value"}]')
+    sourceStream.end()
+
+    targetStream.read()
+      .should.be.deep.equal({property: 'value'})
   })
 })
